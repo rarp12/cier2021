8b854ac174fee6587b6ce30dfd57226df607cc58
Roberto Rocha <rarp12@gmail.com>
2022-02-19 13:17:05 -0500

    Correccion de los tipos de datos
    Se corrigieron los tipos de datos aceptados en el Form de digitacion.

--- VistaControlador/src/main/webapp/resources/js/pf-dialog-crud.js
+++ VistaControlador/src/main/webapp/resources/js/pf-dialog-crud.js
@@ -12,87 +12,101 @@ function handleSubmitRequest(xhr, status, args, dialog) {
 // Esto se valida cuando escribe
 function validateFloatKeyPress(el, evt) {
 
-  var charCode = (evt.which) ? evt.which : event.keyCode;
-  var number = el.value.split('.');
-  // permitir el signo de - (45)
-  if (charCode != 45 && charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
-    return false;
-  }
-  //just one dot
-  if (number.length > 1 && charCode == 46) {
-    return false;
-  }
-  //get the carat position
-  var caratPos = getSelectionStart(el);
-  // no permitir que se ponega el - en una posicion diferente de la inicial
-  if (caratPos > 0 && charCode == 45) {
-    return false;
-  }
-  //No permitir un punto en la posicin inicial
-  
-   if (caratPos == 0 && charCode == 46) {
-    return false;
-  }
-  
-  // no permtir mas de un - en el numero
-  if (charCode == 45 && el.value.charAt(0) == "-") {
-    return false;
-  }
-  var dotPos = el.value.indexOf(".");
-  if (caratPos > dotPos && dotPos > -1 && (number[1].length > 1)) {
-    return false;
-  }
-  return true;
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    var number = el.value.split('.');
+    // permitir el signo de - (45)
+    if (charCode != 45 && charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    //just one dot
+    if (number.length > 1 && charCode == 46) {
+        return false;
+    }
+    //get the carat position
+    var caratPos = getSelectionStart(el);
+    // no permitir que se ponega el - en una posicion diferente de la inicial
+    if (caratPos > 0 && charCode == 45) {
+        return false;
+    }
+    //No permitir un punto en la posicin inicial
+
+    if (caratPos == 0 && charCode == 46) {
+        return false;
+    }
+
+    // no permtir mas de un - en el numero
+    if (charCode == 45 && el.value.charAt(0) == "-") {
+        return false;
+    }
+    var dotPos = el.value.indexOf(".");
+    if (caratPos > dotPos && dotPos > -1 && (number[1].length > 1)) {
+        return false;
+    }
+    return true;
 }
 
 function validateFloatKeyPressNoNegativo(el, evt) {
 
-  var charCode = (evt.which) ? evt.which : event.keyCode;
-  var number = el.value.split('.');
-  // permitir el signo de - (45)
-  if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
-    return false;
-  }
-  //just one dot
-  if (number.length > 1 && charCode == 46) {
-    return false;
-  }
-  //get the carat position
-  var caratPos = getSelectionStart(el);
-  // no permitir que se ponega el - en una posicion diferente de la inicial
-  //No permitir un punto en la posicin inicial
-  
-   if (caratPos == 0 && charCode == 46) {
-    return false;
-  }
-  
-  var dotPos = el.value.indexOf(".");
-  if (caratPos > dotPos && dotPos > -1 && (number[1].length > 1)) {
-    return false;
-  }
-  return true;
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    var number = el.value.split('.');
+    // permitir el signo de - (45)
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    //just one dot
+    if (number.length > 1 && charCode == 46) {
+        return false;
+    }
+    //get the carat position
+    var caratPos = getSelectionStart(el);
+    // no permitir que se ponega el - en una posicion diferente de la inicial
+    //No permitir un punto en la posicin inicial
+
+    if (caratPos == 0 && charCode == 46) {
+        return false;
+    }
+
+    var dotPos = el.value.indexOf(".");
+    if (caratPos > dotPos && dotPos > -1 && (number[1].length > 1)) {
+        return false;
+    }
+    return true;
 }
 
 function validateFloatKeyPressEntero(el, evt) {
 
-  var charCode = (evt.which) ? evt.which : event.keyCode;
-  if (charCode < 48 || charCode > 57) {
-    return false;
-  }
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if (charCode < 48 || charCode > 57) {
+        return false;
+    }
 
-  return true;
+    return true;
+}
+
+function validateAlfanumericoKeyPress(el, evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if (!(charCode > 47 && charCode < 58) &&
+            !(charCode > 64 && charCode < 91) &&
+            !(charCode > 96 && charCode < 123)) {
+        return false;
+    }
+
+    return true;
 }
 
 function getSelectionStart(o) {
-  if (o.createTextRange) {
-    var r = document.selection.createRange().duplicate()
-    r.moveEnd('character', o.value.length)
-    if (r.text == '') return o.value.length
-    return o.value.lastIndexOf(r.text)
-  } else { return o.selectionStart }
+    if (o.createTextRange) {
+        var r = document.selection.createRange().duplicate()
+        r.moveEnd('character', o.value.length)
+        if (r.text == '')
+            return o.value.length
+        return o.value.lastIndexOf(r.text)
+    } else {
+        return o.selectionStart
+    }
 }
 
 // Para cuando pierde el foco
-function validarPuntos (el, evt){
+function validarPuntos(el, evt) {
     alert("A");
 }
